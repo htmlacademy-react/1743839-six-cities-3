7e60a980e9b06918f25533adacb0cf3495defea5
Dimriu <dimriudobrunin@gmail.com>
2026-03-10 16:49:48 +0300

    3.19. Маршрут по тайным тропам_1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
+import { AutorizationStatus, AppRoute } from '../../const';
 
 type NumberOfProposals = {
   offersCount: number;
@@ -7,7 +14,36 @@ type NumberOfProposals = {
 
 function App ({offersCount}: NumberOfProposals): JSX.Element {
   return (
-    <MainPage offersCount={offersCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route
+            index
+            element={<MainPage offersCount={offersCount}/>}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferPage />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={(
+              <PrivateRoute autorizationStatus = {AutorizationStatus.NoAuth}>
+                <FavoritesPage />
+              </PrivateRoute>
+            )}
+          />
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
